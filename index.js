a1c8663025077208f9e1eb8c913c197505edb9f7
Wotax? <santoshdonna2@gmail.com>
2025-07-30 15:48:05 +0545

    Update index.js

--- index.js
+++ index.js
@@ -12,7 +12,8 @@ const client = new Client({
 });
 
 const OWNER_IDS = ['1347611047338709052'];
-const CHANNEL_ID = '1398959087592673370','1399332860175056987';
+const CHANNEL_IDS = ['1398959087592673370', '1399332860175056987'];
+
 const cooldown = new Map();
 
 client.once('ready', () => {
